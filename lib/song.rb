8861493e250e62586a1ea21b7a90264b0c438c57
Micah White <micahjwhite@gmail.com>
2020-04-19 02:44:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,41 +1,45 @@
 class Song
 
-   attr_accessor :name, :artist, :genre
-   @@count = 0
-   @@artists = []
-   @@genres = []
+  attr_accessor :name, :artist, :genre
 
-   def initialize(song_name,artist,genre)
-     @name = song_name
-     @artist = artist
-     @genre = genre
-     @@count += 1
-     @@artists << artist
-     @@genres << genre
-   end
+  @@count = 0
+  @@genres = []
+  @@artists = []
 
-   def self.count
-     @@count
-   end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
 
-   def self.artists
-     @@artists.uniq
-   end
+  def self.count
+    @@count
+  end
 
-   def self.genres
-     @@genres.uniq
-   end
+  def self.genres
+    @@genres.uniq
+  end
 
-   def self.genre_count
-     genre_count = Hash.new(0)
-     @@genres.each {|genre| genre_count[genre] += 1}
-     genre_count
-   end
+  def self.artists
+    @@artists.uniq
+  end
 
-   def self.artist_count
-     artist_count = Hash.new(0)
-     @@artists.each {|artist| artist_count[artist] += 1}
-     artist_count
-   end
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      genre_count[genre] += 1
+    end
+    genre_count
+  end
 
- end
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      artist_count[artist] =+ 1
+    end
+    artist_count
+  end
+end
