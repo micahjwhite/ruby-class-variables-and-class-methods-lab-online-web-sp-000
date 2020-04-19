a932787ec6e36727937dfaa212da72dc0569a0c5
Micah White <micahjwhite@gmail.com>
2020-04-19 02:46:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,9 +38,9 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      artist_count[artist] =+ 1
+      artist_count[artist] += 1
     end
     artist_count
   end
-  
+
 end
